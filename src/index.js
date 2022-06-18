e5acc7aa36e4f57bb7f5c6f9e87c1c71f5e884af
Herman Kudria <h.kudrya@hotmail.com>
2022-06-18 23:21:26 +0200

    feat: write toReadable function test passed 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
-  
+module.exports =function toReadable(number) {
+    const first = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const tens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+    // const hungers = ['one hundred','one hundred','one hundred','one hundred','one hundred','one hundred','one hundred','one hundred','one hundred']
+    let str = ''
+    if (number >= 100){
+       str += first[~~(number/100)] + ' hundred'
+        number = number%100
+    }
+    if( number >= 20 && number < 100){
+        if (str){
+            str += ' '
+        }
+        str += tens[~~(number/10)-2]
+        number = number%10
+    }
+    if (number<20){
+        if (str && number){
+            str += ' '
+        }
+        if(str && number){
+            str += first[number]
+        }
+        if (!str){
+            str += first[number]
+        }
+    }
+    return str
 }
+
+
+
